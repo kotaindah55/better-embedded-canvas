504a7ca4eb8bc217c2c1d22884cafbf7ff8ad8b9
kotaindah55 <kotaindah55@gmail.com>
2026-07-23 12:25:47 +0300

    chore: remove eslint-plugin-jsdoc and tidy up the config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,68 @@
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import eslint, { meta } from '@eslint/js';
+import eslint from '@eslint/js';
 import tslint from 'typescript-eslint';
 import obsidianmdlint from 'eslint-plugin-obsidianmd';
-import jsdoclint from 'eslint-plugin-jsdoc';
 import tsParser from '@typescript-eslint/parser';
 
-export default defineConfig(
-	eslint.configs.recommended,
-	tslint.configs.recommendedTypeChecked,
-	jsdoclint.configs['flat/logical-typescript'],
-	[
-		...obsidianmdlint.configs?.recommendedWithLocalesEn,
-		{
-			ignores: [
-				'**/.deprecated/',
-				'**/@external',
-				'**/node_modules/',
-				'**/libs/',
-				'**/dist/',
-				'**/main.js',
-				'**/esbuild.config.mjs',
-				'**/eslint.config.mjs'
-			],
-			languageOptions: {
-				globals: { ...globals.node },
-				parser: tsParser,
-				parserOptions: {
-					projectService: true,
-					tsconfigRootDir: import.meta.url
-				},
-				ecmaVersion: 'latest',
-				sourceType: 'module'
-			},
-			rules: {
-				'prefer-const': 'off',
-				'no-unused-vars': 'off',
-				'no-unused-labels': 'off',
-				'no-undef': 'off',
-				'no-prototype-builtins': 'off',
-				'no-cond-assign': 'off',
-				'jsdoc/no-undefined-types': 1,
-				'obsidianmd/ui/sentence-case': {
-					ignoreRegex: ['“[\w\d_-]+”']
-				},
-				'@typescript-eslint/unbound-method': 'off',
-				'@typescript-eslint/no-empty-function': 'off',
-				'@typescript-eslint/await-thenable': 'off',
-				'@typescript-eslint/no-unsafe-argument': 'off',
-				'@typescript-eslint/no-unsafe-assignment': 'off',
-				'@typescript-eslint/no-unsafe-member-access': 'off',
-				'@typescript-eslint/no-unsafe-call': 'off',
-				'@typescript-eslint/no-unused-vars': [
-					'error', { args: 'none' },
-				],
+export default defineConfig({
+	files: [
+		'**/*.{ts,mts}'
+	],
+	ignores: [
+		'**/.deprecated/',
+		'**/@external',
+		'**/node_modules/',
+		'**/libs/',
+		'**/dist/',
+		'**/main.js',
+		'**/esbuild.config.mjs',
+		'**/eslint.config.mjs'
+	],
+	extends: [
+		eslint.configs.recommended,
+		...tslint.configs.strictTypeChecked,
+		...tslint.configs.stylisticTypeChecked,
+		...obsidianmdlint.configs.recommended
+	],
+	languageOptions: {
+		globals: { ...globals.node },
+		parser: tsParser,
+		parserOptions: {
+			projectService: true,
+			tsconfigRootDir: import.meta.dirname
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	rules: {
+		'prefer-const': 'off',
+		'no-unused-vars': 'off',
+		'no-unused-labels': 'off',
+		'no-undef': 'off',
+		'no-prototype-builtins': 'off',
+		'no-cond-assign': 'off',
+		'obsidianmd/ui/sentence-case': [
+			'error', { ignoreRegex: ['“[^“”]+”'] }
+		],
+		'obsidianmd/ui/sentence-case-locale-module': [
+			'error', { ignoreRegex: ['“[^“”]+”'] }
+		],
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/await-thenable': 'off',
+		'@typescript-eslint/no-unsafe-argument': 'off',
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+		'@typescript-eslint/no-unsafe-call': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error', { args: 'none' },
+		],
+		'@typescript-eslint/no-confusing-void-expression': [
+			'error', {
+				ignoreVoidOperator: true,
+				ignoreArrowShorthand: true
 			}
-		}
-	]
-);
+		],
+		'@typescript-eslint/no-non-null-assertion': 'warn'
+	}
+});
