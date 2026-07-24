f7537295d35ffcda5b3bd4245cc7b08890c444a3
kotaindah55 <kotaindah55@gmail.com>
2026-07-24 11:01:26 +0300

    chore: patch Obsidian API
    Make `Component.onload()` promisable to avoid `no-misused-promises` error.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -54,7 +54,6 @@ export default defineConfig({
 		'@typescript-eslint/no-unsafe-assignment': 'off',
 		'@typescript-eslint/no-unsafe-member-access': 'off',
 		'@typescript-eslint/no-unsafe-call': 'off',
-		'@typescript-eslint/no-misused-promises': 'off',
 		'@typescript-eslint/no-explicit-any': [
 			'error', { ignoreRestArgs: true }
 		],
