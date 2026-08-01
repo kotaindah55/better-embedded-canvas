81b46356ba8674aba572fed9bf538fde2d594766
kotaindah55 <kotaindah55@gmail.com>
2026-08-01 15:08:55 +0300

    chore: disable some eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -54,6 +54,8 @@ export default defineConfig({
 		'@typescript-eslint/no-unsafe-assignment': 'off',
 		'@typescript-eslint/no-unsafe-member-access': 'off',
 		'@typescript-eslint/no-unsafe-call': 'off',
+		'@typescript-eslint/no-misused-promises': 'off',
+		'@typescript-eslint/no-unsafe-enum-comparison': 'off',
 		'@typescript-eslint/no-explicit-any': [
 			'error', { ignoreRestArgs: true }
 		],
