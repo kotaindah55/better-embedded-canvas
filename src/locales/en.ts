42070414eab0e4c5c5d368039a001d77ddd98ca3
kotaindah55 <kotaindah55@gmail.com>
2026-09-16 09:32:39 +0300

    refactor: update i18n resource

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -2,12 +2,19 @@ export default {
 	tooltipOpenCanvas: 'Open canvas',
 	tooltipEnableInteraction: 'Enable interaction',
 	tooltipDisableInteraction: 'Disable interaction',
-	reloadNotice: {
-		message: 'It is recommended to reload all open notes and canvases after disable or uninstall Better Embedded Canvas plugin (your data are preserved).',
-		buttonReload: 'Reload',
-		buttonDismiss: 'Dismiss'
+	buttonReload: 'Reload',
+	buttonDismiss: 'Dismiss',
+	editor: {
+		suggestion: {
+			noMatchedNode: 'No matched card'
+		}
 	},
-	settings: {
+	notice: {
+		reloadAfterDisable: 'It is recommended to reload all open notes and canvases after disable or uninstall Better Embedded Canvas plugin (your data are preserved).',
+		restartApp: 'Restart the app to ensure the Better Embedded Canvas plugin runs properly.',
+		canvasIsDisabled: 'Canvas plugin is disabled. Enable it under “Settings → Core plugins” and restart the app.'
+	},
+	setting: {
 		showCanvasName: {
 			name: 'Show canvas name',
 			desc: 'Show canvas name as embed title.'
@@ -15,6 +22,17 @@ export default {
 		spaceKeyToPan: {
 			name: 'Press “Space” key to pan',
 			desc: 'Press “Space” key and drag the embedded canvas to move it. Canvas that is embedded inside another canvas, whether directly or via an embedded note, cannot use this method.'
+		},
+		embedNodeContentOnly: {
+			name: 'Embed card content only',
+			desc: 'Embed only content of a canvas card instead of preserving the whole canvas interface. This does not apply to group cards.'
+		},
+		embedGroupContentOnly: {
+			name: 'Embed cards without its group',
+			desc: 'Hide group card while only show the cards inside.'
 		}
+	},
+	settingHeading: {
+		nodeEmbed: 'Card embed'
 	}
 }
--- src/locales/id.ts
+++ src/locales/id.ts
@@ -2,12 +2,19 @@ export default {
 	tooltipOpenCanvas: 'Buka kanvas',
 	tooltipEnableInteraction: 'Nyalakan interaksi',
 	tooltipDisableInteraction: 'Matikan interaksi',
-	reloadNotice: {
-		message: 'Disarankan untuk memuat ulang seluruh catatan dan kanvas yang terbuka setelah menonaktifkan atau menghapus plugin Better Embedded Canvas (data Anda data tetap terjaga).',
-		buttonReload: 'Muat ulang',
-		buttonDismiss: 'Abaikan'
+	buttonReload: 'Muat ulang',
+	buttonDismiss: 'Abaikan',
+	editor: {
+		suggestion: {
+			noMatchedNode: 'Tidak ada kartu yang cocok'
+		}
 	},
-	settings: {
+	notice: {
+		reloadAfterDisable: 'Disarankan untuk memuat ulang seluruh catatan dan kanvas yang terbuka setelah menonaktifkan atau menghapus plugin Better Embedded Canvas (data Anda data tetap terjaga).',
+		restartApp: 'Mulai ulang aplikasi untuk memastikan plugin Better Embedded Canvas berjalan dengan baik.',
+		canvasIsDisabled: 'Plugin Canvas dinonaktifkan. Aktifkan melalui “Pengaturan → Plugin inti” lalu mulai ulang aplikasi.'
+	},
+	setting: {
 		showCanvasName: {
 			name: 'Tampilkan nama kanvas',
 			desc: 'Tampilkan nama kanvas sebagai judul sematan.'
@@ -15,6 +22,17 @@ export default {
 		spaceKeyToPan: {
 			name: 'Tekan “Spasi” untuk menggeser',
 			desc: 'Tekan “Spasi” dan seret kanvas yang disematkan untuk menggesernya. Kanvas yang disematkan di dalam kanvas lain, baik secara langsung maupun melalui catatan yang disematkan, tidak dapat menggunakan metode ini.'
+		},
+		embedNodeContentOnly: {
+			name: 'Hanya sematkan konten dari kartu',
+			desc: 'Cukup sematkan konten kartu tanpa menampilkan antarmuka kanvas. Tidak berlaku pada kartu grup.'
+		},
+		embedGroupContentOnly: {
+			name: 'Sematkan konten grup tanpa grupnya',
+			desc: 'Sembunyikan grup dan cukup tampilkan kartu-kartu di dalamnya.'
 		}
+	},
+	settingHeading: {
+		nodeEmbed: 'Sematan kartu'
 	}
 }
